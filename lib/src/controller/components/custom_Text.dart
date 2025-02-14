8ae53da8052c0f48f8dded81892cea8a2dd99ab2
Arslan Qazi <arslanqazi1999@gmail.com>
2025-02-14 12:58:39 +0500

    Update custom_Text.dart

--- lib/src/controller/components/custom_Text.dart
+++ lib/src/controller/components/custom_Text.dart
@@ -2,18 +2,55 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class CustomText extends StatelessWidget {
-  const CustomText({super.key});
+  String text;
+  double fontSize;
+  FontWeight fontWeight;
+  Color color;
+
+   CustomText({super.key,
+     required this.text,
+     this.fontSize=14,
+     this.fontWeight=FontWeight.normal,
+     this.color=Colors.black,
+
+   });
 
   @override
   Widget build(BuildContext context) {
     return Text(
-      '',
+      text,
       style: GoogleFonts.aBeeZee(
-        fontSize: ,
-        fontWeight: ,
-        color: ,
+        fontSize:fontSize ,
+        fontWeight:fontWeight ,
+        color: color,
       ),
     );
   }
 }
 
+class BoldText extends StatelessWidget {
+  String text;
+  double fontSize;
+  FontWeight fontWeight;
+  Color color;
+
+ BoldText({super.key,
+    required this.text,
+    this.fontSize=18,
+    this.fontWeight=FontWeight.bold,
+    this.color=Colors.black,
+
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: GoogleFonts.aBeeZee(
+        fontSize:fontSize ,
+        fontWeight:fontWeight ,
+        color: color,
+      ),
+    );
+  }
+}
