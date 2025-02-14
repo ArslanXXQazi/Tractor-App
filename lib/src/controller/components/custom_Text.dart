8fd4a6e59461cafdeede77577d15a1954d0f0172
Arslan Qazi <arslanqazi1999@gmail.com>
2025-02-14 12:55:08 +0500

    update

--- lib/src/controller/components/custom_Text.dart
+++ lib/src/controller/components/custom_Text.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class CustomText extends StatelessWidget {
   const CustomText({super.key});
@@ -7,7 +8,11 @@ class CustomText extends StatelessWidget {
   Widget build(BuildContext context) {
     return Text(
       '',
-     
+      style: GoogleFonts.aBeeZee(
+        fontSize: ,
+        fontWeight: ,
+        color: ,
+      ),
     );
   }
 }
