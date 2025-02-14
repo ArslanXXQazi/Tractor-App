ce643a8b2dfd0584ba6ce62bd1c9e5f1861827f0
Arslan Qazi <arslanqazi1999@gmail.com>
2025-02-14 13:16:22 +0500

    Update home_view.dart

--- lib/src/views/home_view/home_view.dart
+++ lib/src/views/home_view/home_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tractor/src/controller/components/custom_Text.dart';
+import 'package:tractor/src/controller/components/home_container.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
@@ -12,10 +13,18 @@ class HomeView extends StatelessWidget {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-
           children: [
-
-
+            HomeContainer(
+                onTap: (){},
+                iconData: Icons.add,
+                text: "Enter Your Data"),
+            SizedBox(height: height*.05,),
+            HomeContainer(
+                onTap: (){},
+                iconData: Icons.remove_red_eye,
+                text: "View Your Data",
+                color: Colors.blue,
+            ),
         ],),
       ),
     );
