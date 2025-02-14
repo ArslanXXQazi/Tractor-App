e58efb88330743ad47efae56995f0dc52e03112a
Arslan Qazi <arslanqazi1999@gmail.com>
2025-02-14 19:31:44 +0500

    Update fetch_view.dart

--- lib/src/views/fetch_view/fetch_view.dart
+++ lib/src/views/fetch_view/fetch_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tractor/src/controller/components/custom_Text.dart';
 
 class FetchView extends StatefulWidget {
   const FetchView({super.key});
@@ -10,6 +11,13 @@ class FetchView extends StatefulWidget {
 class _FetchViewState extends State<FetchView> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        title: BoldText(text: 'Customar Detail'),
+      ),
+    );
   }
 }
