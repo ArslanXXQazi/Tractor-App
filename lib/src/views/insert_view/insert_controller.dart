26c95b9d05a4d286529f280dc7fedc22f9f700eb
Arslan Qazi <arslanqazi1999@gmail.com>
2025-02-14 15:09:51 +0500

    mm

--- lib/src/views/insert_view/insert_controller.dart
+++ lib/src/views/insert_view/insert_controller.dart
@@ -1,3 +1,15 @@
 
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-class InsertController extends
+class InsertController extends GetxController
+{
+
+  TextEditingController nameController=TextEditingController();
+  TextEditingController timeController=TextEditingController();
+  TextEditingController priceController=TextEditingController();
+  TextEditingController totalPriceController=TextEditingController();
+  TextEditingController dateController=TextEditingController();
+
+
+}
