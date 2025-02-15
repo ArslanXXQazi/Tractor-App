c79bcf13f097135aa72b9027c8d642f24e6e2836
Arslan Qazi <arslanqazi1999@gmail.com>
2025-02-15 20:46:09 +0500

    Update custom_button.dart

--- lib/src/controller/components/custom_button.dart
+++ lib/src/controller/components/custom_button.dart
@@ -2,23 +2,30 @@ import 'package:flutter/material.dart';
 import 'package:tractor/src/controller/components/custom_Text.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({super.key});
+
+  String text;
+  Color color;
+  VoidCallback onTap;
+
+ CustomButton({super.key,
+   required this.onTap,
+   required this.text,
+   this.color=Colors.green
+ });
 
   @override
   Widget build(BuildContext context) {
     final height= MediaQuery.sizeOf(context).height;
     final width= MediaQuery.sizeOf(context).width;
     return  ElevatedButton(
-        onPressed: ()
-        {
-        },
+        onPressed: onTap,
         style: ElevatedButton.styleFrom(
-          backgroundColor: ,
+          backgroundColor: color,
           minimumSize: Size(width*1, height*.07),
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(10),
           ),
         ),
-        child: BoldText(text: 'Add Data',color: Colors.white,));
+        child: BoldText(text: text,color: Colors.white,));
   }
 }
