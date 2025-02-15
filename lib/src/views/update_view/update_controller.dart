a342e8ba52d65acc1da0630d16488727bfdf6b77
Arslan Qazi <arslanqazi1999@gmail.com>
2025-02-15 22:26:42 +0500

    update

--- lib/src/views/update_view/update_controller.dart
+++ lib/src/views/update_view/update_controller.dart
@@ -16,7 +16,30 @@ class UpdateController extends GetxController
 
   var isLoading = false.obs;
 
+  void updateData(int id) async {
+    isLoading.value = true;
 
+    MainModel updatedModel = MainModel(
+      id: id,
+      customar_name: nameController.text,
+      time: timeController.text,
+      price: priceController.text,
+      total_price: totalPriceController.text,
+      date: dateController.text,
+      description: descriptionController.text,
+    );
+
+    int result = await dbClass.updateData(id: id, model: updatedModel);
+
+    if (result > 0) {
+      Get.snackbar("Success", "Data Updated Successfully", backgroundColor: Colors.green, colorText: Colors.white);
+      Get.back(); // Update hone ke baad wapas FetchView pe chale jaye
+    } else {
+      Get.snackbar("Error", "Failed to Update Data", backgroundColor: Colors.red, colorText: Colors.white);
+    }
+
+    isLoading.value = false;
+  }
 
 
 }
