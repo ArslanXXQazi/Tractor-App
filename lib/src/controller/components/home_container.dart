1f93cc9c3c5829f4c3fe92231203e59f623aae6f
Arslan Qazi <arslanqazi1999@gmail.com>
2025-02-20 21:44:46 +0500

    a;jevb

--- lib/src/controller/components/home_container.dart
+++ lib/src/controller/components/home_container.dart
@@ -5,10 +5,8 @@ class HomeContainer extends StatelessWidget {
   IconData iconData;
   String text;
   VoidCallback onTap;
- // Color color;
-   HomeContainer({super.key,required this.onTap,required this.iconData,required this.text,
-    // this.color=Colors.green
-   });
+  Color color;
+   HomeContainer({super.key,required this.onTap,required this.iconData,required this.text,this.color=Colors.green});
 
   @override
   Widget build(BuildContext context) {
@@ -21,17 +19,7 @@ class HomeContainer extends StatelessWidget {
         height: height*.35,
         width: width*.8,
         decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              Color(0xFF434343), // Charcoal Gray
-              Color(0xFF6D6027), // Warm Brownish Gold
-              Color(0xFFD3A42F), // Royal Gold
-              Color(0xFFFFD700), // Shiny Gold
-              Color(0xFFB8860B), // Dark Gold
-            ],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-          ),
+          color: color,
           borderRadius: BorderRadius.circular(10),
         ),
         child: Column(
