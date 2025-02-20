7b75d80f27e8fee53a71d787d11829e16dc94f50
Arslan Qazi <arslanqazi1999@gmail.com>
2025-02-20 21:06:02 +0500

    skd n

---
+++ lib/src/controller/components/divider.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class Divider extends StatelessWidget {
+  const Divider({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
