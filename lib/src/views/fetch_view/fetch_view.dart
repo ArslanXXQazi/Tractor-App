cea40948d3297cc010e0d3a5d921e28d6307bdff
Arslan Qazi <arslanqazi1999@gmail.com>
2025-02-20 22:06:07 +0500

    Update fetch_view.dart

--- lib/src/views/fetch_view/fetch_view.dart
+++ lib/src/views/fetch_view/fetch_view.dart
@@ -227,7 +227,7 @@ class _FetchViewState extends State<FetchView> {
             ),
           );
         },
-      ).animate().shimmer(duration: Duration(seconds: 2)),
+      ).animate().saturate(duration: Duration(seconds: 2)),
     );
   }
 }
